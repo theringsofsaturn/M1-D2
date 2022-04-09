4ce05850c873f0284407f947200359bd27842395
theringsofsaturn <emiliankasemi@gmail.com>
2022-04-10 01:59:17 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,74 @@
+
+# JavaScript Exercises 
+
+# Module 1 / Day 1 at Strive School (EPICODE)
+# Answers in the index.js file
+
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+
+/* EXERCISE 1
+Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 2
+Try to describe what a variable is, in your own words.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 3
+Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 4
+Create a variable named x containing the number 12.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 5
+Create a variable called name containing the string John Doe.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 6
+Execute a subtraction between the number 12 and the variable x, which is storing the value 12.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 7
+Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
+Verify that name1 is different from name2. 
+Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2).
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 8
+Create a variable named x (its value must be less than 10). Write the code to print the literal value of x (ex.: if x is 1 print "one", if 5 print "five" etc.).
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 9
+[Extra] Insert a value in a variable based on the resut of a ternary if (topic not covered during lesson, try to search it by yourself, tomorrow morning we'll discuss it together ;) )
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
--- index.js
+++ index.js
@@ -1,180 +1,145 @@
-// an ALGHORITM is a finite set of steps for solving a particular problem
+/* EXERCISE 1
+Enumerate and describe the main datatypes in JavaScript. Try to explain the concepts as you were speaking to a 12 years old.
+*/
 
+/* WRITE YOUR ANSWER HERE */
 
-// DATA TYPES
+/* A box that contains something. A placeholder for an unknown value, or we can give the value we want. You can open the box to find out what is inside, or even put something else into the box */
 
+/*
+NUMBERS
+5
+5.5
+45.24545654
 
-// NUMBERS
-// 5
-// 5.5
-// 45.24545654
 
+STRINGS
+"hello strivers"
+'hello strivers'
+`hello strivers`
 
-// STRINGS
-// "hello strivers"
-// 'hello strivers'
-// `hello strivers`
 
-
-// BOOLEANS
-// true
-// false
-// 3 > 2 --> true
-// 4 > 10 --> false
-// 3 === 3 --> true
-// 10 === 5 --> false
-// 5 === '5' --> false
-// 'strive' === 'strive' --> true
-// === --> checks if they are the same in VALUE and TYPE
-// 5 == '5' --> true DO NOT USE THIS
-// '' --> false
-// 'nando' --> true 
-
-
-// 3 + 4 = 7
-// '3' + '4' = '34'
-
-
-// SPECIAL ONES
-// undefined -> the value is not there yet
-// null -> not existing, the total absence of value
-
-
-// VARIABLES
-let firstValue = 10 // this is a variable creation, because I used let
-// single = means ASSIGNMENT
-// now firstValue is 10
-
-
-console.log('!!!', firstValue)
-
-
-let secondValue
-// this notation is called camelCase
-firstValue = 20
-// now firstValue is 20
-
-
-console.log('???', firstValue)
-
-
-// firstValue is now 20, not 10 any more
-secondValue = 'stefano'
-
-
-let thirdValue
-// now thirdValue is undefined
-
-
-let sum = 30 + firstValue // actually I'm summing up 30 + 20
-// sum has a value of 50
-
-
-console.log('sum is', sum)
-
-
-// var oldStyleVariable = 0 // DO NOT USE VAR
-const thisValueWillNotChangeEver = 'sarah'
-// thisValueWillNotChangeEver = 'Jacob' // THIS WILL TRIGGER AN ERROR
-
-
-let difference = sum - 25
-console.log('sum after difference', sum)
-
-
-console.log('this is difference', difference)
-
-
-let multiply = 10 * 5
-let division = 50 / 10
-
-
-let modulo = 25 % 5 // 0 so the remainder of the division
-
-
-let result = (25 + 10) * 3
-
-
-let multipleStrings = 'hello' + ' ' + 'Mykola'
-console.log(multipleStrings)
-let thisIsNotANumberAgain = '100'
-
-
-console.log(thisIsNotANumberAgain + 100)
-console.log(typeof thisIsNotANumberAgain)
-
-
-let uppercaseString = multipleStrings.toUpperCase()
-console.log(uppercaseString)
-let thisIsNowLowerCase = uppercaseString.toLowerCase()
-console.log(thisIsNowLowerCase)
-
-
-// true
-// false
-
-
-// console.log(5 === 10)
-console.log(6 >= 7)
-console.log('stefano' !== 'shakira')
-
-
-// && --> AND
-// || --> OR
-
-
-console.log(5 === 5 && 10 === 10)
-// AND will return TRUE just if the expressions before and after
-// are BOTH true
-
-
-// TRUE && TRUE --> TRUE
-// TRUE && FALSE --> FALSE
-// FALSE && TRUE --> FALSE
-// FALSE && FALSE --> FALSE
-
-
-console.log(5 === 5 || 10 === 1)
-// OR will return TRUE if at least ONE of the conditions are TRUE
-
-
-// TRUE || TRUE --> TRUE
-// TRUE || FALSE --> TRUE
-// FALSE || TRUE --> TRUE
-// FALSE || FALSE --> FALSE
-
-
-console.log((4 < 5 && 5 > 10) || 5 !== 5)
-
-
-// the cart has a total
-// you have a threshold for giving free shipping
-// if the total is > than the threshold, shipping is free
-// the grand total will be the cart total
-// if we don't reach the threshold, we need to add shipping cost
-
-
-const cartTotal = 60
-
-
-const freeShippingThreshold = 50
-const shippingCost = 10
-
-
-let grandTotal
-
-
-if(cartTotal > freeShippingThreshold) {
-// the grand total will be our cartTotal
-// yey, I'm eligible for free shipping!!
-    console.log("FREE SHIPPING!")
-    grandTotal = cartTotal
+BOOLEANS
+true
+false
+3 > 2 --> true
+4 > 10 --> false
+3 === 3 --> true
+10 === 5 --> false
+5 === '5' --> false
+'strive' === 'strive' --> true
+=== --> checks if they are the same in VALUE and TYPE
+5 == '5' --> true DO NOT USE THIS
+'' --> false
+'nando' --> true 
+
+
+3 + 4 = 7
+'3' + '4' = '34'
+
+
+SPECIAL ONES
+undefined -> the value is not there yet
+null -> not existing, the total absence of value
+*/
+
+/* EXERCISE 2
+Try to describe what a variable is, in your own words.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 3
+Write the code to execute an addition (a sum) of the numbers 12 and 20.
+*/
+
+let result = 12 + 20;
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 4
+Create a variable named x containing the number 12.
+*/
+
+let x = 12;
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 5
+Create a variable called name containing the string John Doe.
+*/
+
+let name = "John Doe";
+// let name = 'John Doe'
+// let name = `John Doe`
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 6
+Execute a subtraction between the number 12 and the variable x, which is storing the value 12.
+*/
+
+let ex6result = 12 - x;
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 7
+Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
+Verify that name1 is different from name2. 
+Verify then, that name1 and name2 are equals if both lowercase (without changing the value of name2).
+*/
+
+let name1 = "john";
+let name2 = "John";
+console.log(name1 === name2);
+console.log(name1.toLowerCase() === name2.toLowerCase());
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 8
+Create a variable named x (its value must be less than 10). 
+Write the code to print the literal value of x (ex.: if x is 1 print "one", if 5 print "five" etc.).
+*/
+
+x = 6;
+if (x === 1) {
+  console.log("one");
+} else if (x === 2) {
+  console.log("two");
+} else if (x === 3) {
+  console.log("three");
+} else if (x === 4) {
+  console.log("four");
+} else if (x === 5) {
+  console.log("five");
+} else if (x === 6) {
+  console.log("six");
+} else if (x === 7) {
+  console.log("seven");
+} else if (x === 8) {
+  console.log("eight");
+} else if (x === 9) {
+  console.log("nine");
 } else {
-    // if not, the total will be cartTotal + shippingCost
-    // we're not eligible for free shipping!
-    console.log("I'm sorry, no free shipping for you!")
-    grandTotal = cartTotal + shippingCost
+  console.log("not a valid number!");
 }
 
+/* WRITE YOUR CODE HERE */
 
-console.log('GRANDTOTAL IS', grandTotal)
+/* EXERCISE 9
+[Extra] Insert a value in a variable based on the resut of a ternary if (topic not covered during lesson, try to search it by yourself, tomorrow morning we'll discuss it together ;) )
+*/
+
+let ternaryExample = x > 10 ? true : false;
+// if (x > 10){
+//     ternaryExample = true
+// }
+// else{
+//     ternaryExample = false
+// }
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Upload the .js file on Eduflow before 5PM CET. In the next days we'll also learn how to use GIT!
+*/
